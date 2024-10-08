49b49f20101c8960fe3d0ba2c896bd326def5f07
CamiloGato <cm.andres022@hotmail.com>
2024-10-08 13:46:35 -0500

    Update: Added MEC

--- Assets/_Systems/CarController/CarSystem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CarController
-{
-    public abstract class CarSystem
-    {
-        public abstract void StartSystem();
-        public abstract void UpdateSystem();
-        public abstract void EndSystem();
-    }
-}
